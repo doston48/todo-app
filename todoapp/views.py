771dc7d07608fac67c94b88d9ae7468b9fcf0b43
doston <dost7ony6@gmail.com>
2026-01-18 12:18:49 -0500

    changes

--- todoapp/views.py
+++ todoapp/views.py
@@ -3,24 +3,22 @@ from django.http import HttpResponse
 from .models import todo
 
 def todo_list(request):
-    todos = todo.objects.all()
-    response =  ""
+    todos = todo.objects.alL()
+
     
 # models.py U
 
-    for todo in todos:
-          status = "Done" if todo.is_completed else "Not Done"
-          response += f"{todo.title} - {status}"
+   
+    return render(request, 'todoapp/base.html')
     
-    
-    return HttpResponse(response)
+
 
 def pending_todos(request):
     todos = todo.objects.filter(is_completed=False)
     response = ""
         
     for todo in todos:
-        response += f"{todo.title}"\n{todo.priority}\n{todo.due_date}\n
+        response += f"{todo.title}\n{todo.priority}\n{todo.due_date}\n"
  
 
 def completed_todos(request):
